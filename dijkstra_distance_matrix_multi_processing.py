5b79c29e659a6b2f3b8a8f3bd25ab86dbce17aad
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2023-02-28 17:25:39 +0100

    Update dijkstra_distance_matrix_multi_processing.py

--- dijkstra_distance_matrix_multi_processing.py
+++ dijkstra_distance_matrix_multi_processing.py
@@ -5,7 +5,6 @@ Originally posted by Wei-Meng Lee on towardsdatascience
 For graph neural networks fastest Dijkstra routes can be found using OpenStreetMap API. 
 https://towardsdatascience.com/visualization-in-python-finding-routes-between-points-2d97d4881996
 Multi processing on a CPU or using a GPU is advised as the the process is very resource intensive.
-place = 'Munich, Bavaria, Germany'
 
 Using the six sensors from the RAD dataset as an example to connect the six sensors over the road network
 '''
@@ -17,50 +16,60 @@ import pandas as pd
 import osmnx as ox
 import networkx as nx
 
-
 # Using the cache accelerates processing for a large map
-ox.config(log_console=True, use_cache=True)
+ox.config(log_console=True, use_cache=True, timeout=1000)
 
-# find shortest route based on the mode of travel
+# find shortest route based on the mode of travel and place
+def create_graph(place, mode):
 
-#place = 'Los Angeles, California, United States'
-place = 'Munich, Bavaria, Germany'
+    # request the graph from the inputs
+    graph = ox.graph_from_place(place, network_type=mode)
 
-# 'drive', 'bike', 'walk'
-mode = 'bike'
+    # get the largest strongly connected component
+    scc_generator = nx.strongly_connected_components(graph)
+    largest_scc = max(scc_generator, key=len)
 
-# request the graph from the inputs once!!!
-graph = ox.graph_from_place(place, network_type = mode)
+    # create a new graph containing only the largest strongly connected component
+    graph = graph.subgraph(largest_scc)
+
+    return graph
 
 ##### Interface to OSMNX
-def generate_adjacency_matrix(df, df_row, row_index):
+def generate_adjacency_matrix(df, df_row, row_index, graph):
+
+    # Data import path
+    OS_PATH = os.path.dirname(os.path.realpath('__file__'))
 
     # Create the adjacency matrix
-    matrix = [["DETEKTOR_ID_X, DETEKTOR_ID_Y, DISTANCE"]]
+    matrix = [["detid_X, detid_Y, DISTANCE"]]
 
     # nested for loop to find the distances between all the sensors for every sensor
     i=0;
-    for index, detector in islice(df_row.iterrows(), 0, len(list(df_row.DETEKTOR_ID))):
+    for index, detector in islice(df_row.iterrows(), 0, len(list(df_row.detid))):
         j=0;
         for l, each_detector in df.iterrows():
-            
+
             # coordinates from the current sensor
-            start_latlng = (float(detector["LATITUDE"]), float(detector["LONGITUDE"]))
+            start_latlng = (float(detector["lat"]), float(detector["long"]))
             # coordinates belonging to the destination sensor
-            end_latlng = (float(each_detector["LATITUDE"]), float(each_detector["LONGITUDE"]))
+            end_latlng = (float(each_detector["lat"]), float(each_detector["long"]))
 
-            # find the nearest node to the current sensor
-            orig_node = ox.get_nearest_node(graph, start_latlng)
-            # find the nearest node to the destination sensor
-            dest_node = ox.get_nearest_node(graph, end_latlng)
+            # Try catch because sometimes a node isn't on the graph
+            try:
+                # find the nearest node to the current sensor
+                orig_node = ox.get_nearest_node(graph, start_latlng)
+                # find the nearest node to the destination sensor
+                dest_node = ox.get_nearest_node(graph, end_latlng)
 
-            #find the shortest path method dijkstra or bellman-ford
-            shortest_route_distance = nx.shortest_path_length(graph, orig_node,dest_node, weight="length", method="dijkstra")
+                #find the shortest path method dijkstra or bellman-ford
+                shortest_route_distance = nx.shortest_path_length(graph, orig_node,dest_node, weight="length", method="dijkstra")
+            except nx.NetworkXNoPath:
+                shortest_route_distance = 0
     
 
-            matrix.append([str(detector["DETEKTOR_ID"]) + "," + str(each_detector["DETEKTOR_ID"]) + "," + str(float(shortest_route_distance))])
+            matrix.append([str(detector["detid"]) + "," + str(each_detector["detid"]) + "," + str(float(shortest_route_distance))])
 
-            #print(matrix)
+            print(matrix)
             
             j=+1;
         
@@ -70,9 +79,9 @@ def generate_adjacency_matrix(df, df_row, row_index):
     matrix = np.asarray(matrix)
 
     # Save the dijkstra rad sensors distance matrix
-    np.savetxt(OS_PATH + "/output/bicycle_adjacency_matrix_" + str(row_index) + ".csv", matrix, delimiter=",", fmt='%s')
+    np.savetxt(OS_PATH + "/munich_adjacency_matrix_" + str(row_index) + ".csv", matrix, delimiter=",", fmt='%s')
 
-def combine_csv_files():
+def combine_csv_files(df):
 
     # Data import path
     OS_PATH = os.path.dirname(os.path.realpath('__file__'))
@@ -82,7 +91,7 @@ def combine_csv_files():
 
     # Loop through all the CSV files created by generate_adjacency_matrix
     for i in range(1, len(df) + 1):
-        file_path = OS_PATH + "/output/bicycle_adjacency_matrix_" + str(i) + ".csv"
+        file_path = OS_PATH + "/munich_adjacency_matrix_" + str(i) + ".csv"
         if os.path.exists(file_path):
             # Read the CSV file into a dataframe and append it to the list
             df = pd.read_csv(file_path)
@@ -92,35 +101,38 @@ def combine_csv_files():
     result = pd.concat(dfs)
 
     # Save the concatenated dataframe as a CSV file
-    result.to_csv(OS_PATH + "/output/combined_adjacency_matrix.csv", index=False)
+    result.to_csv(OS_PATH + "/munich_adjacency_matrix.csv", index=False)
+
 
 if __name__ == '__main__':
 
     # Data import path
     OS_PATH = os.path.dirname(os.path.realpath('__file__'))
-    SENSORS_CSV   = OS_PATH + '/data/munich_bicycle_sensors.csv'
+    SENSORS_CSV   = OS_PATH + '/munich_sensors.csv'
 
     # Data Import Path
     df = pd.read_csv(SENSORS_CSV)
 
     # Keep only relevant columns
-    df = df.loc[:, ("DETEKTOR_ID","LATITUDE", "LONGITUDE")]
+    df = df.loc[:, ("detid","lat", "long")]
 
     # Remove missing geocoordinates
-    df.dropna(subset=['LATITUDE'], how='all', inplace=True)
-    df.dropna(subset=['LONGITUDE'], how='all', inplace=True)
+    df.dropna(subset=['lat'], how='all', inplace=True)
+    df.dropna(subset=['long'], how='all', inplace=True)
 
     # Remove missing sensor ids
-    df.dropna(subset=['DETEKTOR_ID'], how='all', inplace=True)
+    df.dropna(subset=['detid'], how='all', inplace=True)
 
     num_processes = len(df)
 
-    print(num_processes)
+    # Create the networkx graph
+    # 'drive', 'bike', 'walk'
+    graph = create_graph("Munich, Bavaria, Germany", "drive")
 
     # Create a process for each row in the df dataframe
     processes = []
     for i, row in df.iterrows():
-        p = mp.Process(target=generate_adjacency_matrix, args=(df, df[i:i+1], i+1))
+        p = mp.Process(target=generate_adjacency_matrix, args=(df, df[i:i+1], i+1, graph))
         p.start()
         processes.append(p)
         
@@ -129,4 +141,4 @@ if __name__ == '__main__':
 
 
     # Combine the CSV files
-    combine_csv_files()
+    combine_csv_files(df)
